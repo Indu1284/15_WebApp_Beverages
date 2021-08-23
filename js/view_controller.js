11c870b14d9f9cebd12d30d12a04ed5123a56564
Peter <schmidt.multimedia@gmail.com>
2021-08-23 11:08:59 +0200

    03 Datenmodelle / gui + data

--- js/view_controller.js
+++ js/view_controller.js
@@ -5,16 +5,15 @@
     Einlesen Daten von Webseite :: toDo
 	Check Daten :: toDo
     Btn. Trigger :: toDo
-    Business-Logic (Alter --> Getränk) :: toDo
+    Business-Logic (Alter --> Getränk) :: check!
     Bild austauschen :: check!
 */
 
 // Modul Ablaufsteuerung | Test:
-// controller()
+controller()
 function controller() {
-    ausgabe(updateImg(checkAge()));
+    ausgabe(updateImg(checkAge(getInput())));
 }
-
 
 
 // Trigger - Btn 
@@ -30,7 +29,10 @@ function controller() {
 
 
 // Modul: Eingabe | Test:
-
+// ausgabe(getInput());
+function getInput() {
+    return 2;
+}
 
 //Modul: Business-Logic (Mapping) | Test:
 // ausgabe(checkAge(2));
@@ -42,25 +44,25 @@ function controller() {
 function checkAge(age) {
     switch (true) 
     {
-        case (age >= 0) && (age <= 5):
-            return "milch";
-        case (age >= 6) && (age <= 12):
-            return "saft";
-        case (age >= 13) && (age <= 17):
-            return "cola";
-        case (age >= 18) && (age <= 130):
-            return "wein";
+        case (age >= data.milk.lower) && (age <= data.milk.upper):
+            return data.milk.bev;
+        case (age >= data.juice.lower) && (age <= data.juice.upper):
+            return data.juice.bev;
+        case (age >= data.cola.lower) && (age <= data.cola.upper):
+            return data.cola.bev;
+        case (age >= data.wine.lower) && (age <= data.wine.upper):
+            return data.wine.bev;
         default:
-            return "tee";
+        return data.default.bev;
     }
 }
 
 // Modul: Bild aktualisieren | Test:
-//ausgabe(updateImg("cola"));
+// ausgabe(updateImg("cola"));
 //ausgabe(updateImg("milch"));
 function updateImg(imgName) {
     let img = document.getElementById("bevImg");
-    img.src = "./bilder/" + imgName + ".jpg";
+    img.src = gui.img.path + imgName + gui.img.ext;
     return imgName; // monitoring
 }
 
